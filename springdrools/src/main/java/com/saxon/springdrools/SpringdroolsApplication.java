a4668324f22ae70bd49919c128c4eeaf713d4817
oushu1zhangxiang1 <zhang.xiangxuan@oushu.io>
2020-10-21 17:09:47 +0800

    presearch finished

---
+++ springdrools/src/main/java/com/saxon/springdrools/SpringdroolsApplication.java
@@ -0,0 +1,13 @@
+package com.saxon.springdrools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdroolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdroolsApplication.class, args);
+	}
+
+}
---
+++ springdrools/src/test/java/com/saxon/springdrools/SpringdroolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saxon.springdrools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdroolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
